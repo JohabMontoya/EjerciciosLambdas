ecb1ee3906ef120b4ff260a2f59c851239f73b7e
johab <johab.montoya@uabc.edu.mx>
2026-04-15 12:03:22 -0700

    Arreglos al método multiplicador

--- src/TareaLambdas.java
+++ src/TareaLambdas.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 
 public class TareaLambdas {
     public static void main(String[] args) {
+
         ArrayList<Integer> numeros = new ArrayList<>();
         numeros.add(4);
         numeros.add(5);
@@ -10,10 +11,24 @@ public class TareaLambdas {
         multiplicador(numeros, factor);
     }
 
+    // Multiplica cada elemento de la lista por un factor dado
     public static void multiplicador(ArrayList<Integer> numeros, int factor){
-       numeros.replaceAll(n -> n * factor);
-       System.out.println(numeros);
+        // Imprime la lista original y el factor
+        System.out.println("Esta es la lista sin modificar.");
+        System.out.println(numeros);
+        System.out.println("Este es el factor.");
+        System.out.println(factor);
+
+        // Recorre la lista y reemplaza cada número por su valor * factor
+        numeros.replaceAll(n -> n * factor);
+
+        // Imprime la lista ya modificada
+        System.out.println("Esta es la lista modificada.");
+        System.out.println(numeros);
     }
+
+
+
 
 
 }
