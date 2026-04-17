7eebce8c7cf9d6d9fdcc07ee1cce7a6e454592ca
johab <johab.montoya@uabc.edu.mx>
2026-04-17 12:18:31 -0700

    Arreglos en el método filtroSelectivo, como hacer que reciba un char y se adapta para que pueda funcionar con este mismo

--- src/TareaLambdas.java
+++ src/TareaLambdas.java
@@ -12,7 +12,7 @@ public class TareaLambdas {
         remover.add("Nombre");
         remover.add("Hola");
         remover.add("Cirujano");
-        String letra = "n";
+        char letra = 'n';
         int tamanoCadena = 5;
         int factor = 5;
         multiplicador(numeros, factor);
@@ -35,16 +35,18 @@ public class TareaLambdas {
         System.out.println(numeros);
     }
 
-    public static void filtroSelectivo(ArrayList<String> remover, String letra, int tamanoCadena){
+    public static void filtroSelectivo(ArrayList<String> remover, char letra, int tamanoCadena){
         // Muestra qué criterios de eliminación se van a aplicar
         System.out.println("Se eliminaran palabras que empiecen por: '" + letra + "'. O tengan una longitud de: " + tamanoCadena);
         // Imprime la lista antes de filtrar
         System.out.println("Cadena original: " + remover);
         // Pasa la letra a minúsculas para comparar sin importar mayúsculas
-        String l = letra.toLowerCase();
+        char l = Character.toLowerCase(letra);
         // Elimina si: empieza por la letra O si su longitud es menor a tamanoCadena
-        remover.removeIf(n -> n.toLowerCase().startsWith(l) || n.length() < tamanoCadena);
+        remover.removeIf(s -> s.toLowerCase().startsWith(String.valueOf(l)) || s.length() < tamanoCadena);
         // Imprime la lista después de filtrar
         System.out.println("Cadena con la letra " + letra + " removida: " + remover);
     }
+
+
 }
