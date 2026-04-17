3d36535590906239682c395fdd316a028933b478
johab <johab.montoya@uabc.edu.mx>
2026-04-17 14:39:42 -0700

    Se agrega método conversorMayuscula, que recibe un ArrayList, lo imprime y luego crea un nuevo ArrayList que tenga el mismo valor que palabras, pero con el método map, lo transforma a mayúsculas, y lo recolecta los datos en un nuevo ArrayList llamado nuevo, para posteriormente imprimirlos

--- src/TareaLambdas.java
+++ src/TareaLambdas.java
@@ -1,10 +1,13 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class TareaLambdas {
     public static void main(String[] args) {
 
         ArrayList<Integer> numeros = new ArrayList<>();
         ArrayList<String> remover = new ArrayList<>();
+        ArrayList<String> palabras = new ArrayList<>();
         numeros.add(4);
         numeros.add(5);
         numeros.add(6);
@@ -12,11 +15,15 @@ public class TareaLambdas {
         remover.add("Nombre");
         remover.add("Hola");
         remover.add("Cirujano");
+        palabras.add("Misterio");
+        palabras.add("hermano");
+        palabras.add("mono");
         char letra = 'n';
         int tamanoCadena = 5;
         int factor = 5;
         multiplicador(numeros, factor);
         filtroSelectivo(remover, letra, tamanoCadena);
+        conversorMayuscula(palabras);
     }
 
     // Multiplica cada elemento de la lista por un factor dado
@@ -38,15 +45,27 @@ public class TareaLambdas {
     public static void filtroSelectivo(ArrayList<String> remover, char letra, int tamanoCadena){
         // Muestra qué criterios de eliminación se van a aplicar
         System.out.println("Se eliminaran palabras que empiecen por: '" + letra + "'. O tengan una longitud de: " + tamanoCadena);
+
         // Imprime la lista antes de filtrar
         System.out.println("Cadena original: " + remover);
+
         // Pasa la letra a minúsculas para comparar sin importar mayúsculas
         char l = Character.toLowerCase(letra);
+
         // Elimina si: empieza por la letra O si su longitud es menor a tamanoCadena
         remover.removeIf(s -> s.toLowerCase().startsWith(String.valueOf(l)) || s.length() < tamanoCadena);
+
         // Imprime la lista después de filtrar
         System.out.println("Cadena con la letra " + letra + " removida: " + remover);
     }
 
-
+    public static ArrayList conversorMayuscula(ArrayList<String> palabras){
+        // Muestra la cadena original antes de modificarla
+        System.out.println("Palabras para transformar a mayusculas: " + palabras);
+        ArrayList<String> nuevo = palabras.stream()
+                .map(String::toUpperCase)
+                .collect(Collectors.toCollection(ArrayList :: new));
+        System.out.println("Palabras en mayusculas: " + nuevo);
+        return nuevo;
+    }
 }
